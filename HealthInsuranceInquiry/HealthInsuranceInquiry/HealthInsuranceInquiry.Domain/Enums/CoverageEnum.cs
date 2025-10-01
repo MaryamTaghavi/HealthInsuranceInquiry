e7d0f17550445b9c8efde93ccb6561e1ab8005b0
MaryamTaghavi <m.taghavi.ce@gmail.com>
2025-10-01 18:29:54 +0330

    addCoverageEnum

---
+++ HealthInsuranceInquiry/HealthInsuranceInquiry/HealthInsuranceInquiry.Domain/Enums/CoverageEnum.cs
@@ -0,0 +1,19 @@
+﻿namespace HealthInsuranceInquiry.Domain.Enums;
+
+public enum CoverageEnum
+{
+    /// <summary>
+    /// پوشش جراحی
+    /// </summary>
+    Surgical = 1,
+
+    /// <summary>
+    /// پوشش دندان پزشکی
+    /// </summary>
+    Dental = 2,
+
+    /// <summary>
+    /// پوشش بستری
+    /// </summary>
+    Inpatient = 3
+}
