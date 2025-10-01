c5e20704eb4cddb631499ee9fb7e437c9f13f5b8
MaryamTaghavi <m.taghavi.ce@gmail.com>
2025-10-01 18:34:59 +0330

    addCoverageHelper

---
+++ HealthInsuranceInquiry/HealthInsuranceInquiry/HealthInsuranceInquiry.Application/Helper/CoverageHelper.cs
@@ -0,0 +1,17 @@
+﻿using HealthInsuranceInquiry.Domain.Enums;
+
+namespace HealthInsuranceInquiry.Application.Helper;
+
+public static class CoverageHelper
+{
+    public static (decimal Min, decimal Max) GetLimits(this CoverageEnum coverage)
+    {
+        return coverage switch
+        {
+            CoverageEnum.Surgical => (5000m, 500_000_000m),
+            CoverageEnum.Dental => (4000m, 400_000_000m),
+            CoverageEnum.Inpatient => (2000m, 200_000_000m),
+            _ => throw new ArgumentOutOfRangeException(nameof(coverage), "Unknown coverage type")
+        };
+    }
+}
