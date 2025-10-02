42b1e85ae226b4d4e097dfc7dcb0d80a2413449a
MaryamTaghavi <m.taghavi.ce@gmail.com>
2025-10-02 11:16:48 +0330

    addSettingInProgram

--- HealthInsuranceInquiry/HealthInsuranceInquiry/HealthInsuranceInquiry/Program.cs
+++ HealthInsuranceInquiry/HealthInsuranceInquiry/HealthInsuranceInquiry/Program.cs
@@ -1,10 +1,32 @@
+using HealthInsuranceInquiry.Application.Features.CreateInsuranceRequest;
+using HealthInsuranceInquiry.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseInMemoryDatabase("MyTestDb"));
+
+builder.Services.AddSwaggerGen(c =>
+{
+    var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    c.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
+});
 
 builder.Services.AddControllers();
+builder.Services.AddMediatR(cfg =>
+{
+    cfg.RegisterServicesFromAssembly(typeof(CreateInsuranceRequestCommand).Assembly);
+});
 
 var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
 // Configure the HTTP request pipeline.
 
